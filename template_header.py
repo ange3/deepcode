a81bcf54e5349a837d8d1d1b0b5dedaf18b8e054
Lisa Wang <lisa1010@stanford.edu>
2016-01-23 14:19:31 -0800

    added a template header file to make our python files look nice. Also started a file for our first rnn.

---
+++ template_header.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# File: pipeline.py
+# @Author: Lisa Wang
+# @created: Nov 08 2015
+#
+#==============================================================================
+# DESCRIPTION:
+# This is a template for a python file comment header. 
+# Kudos to Isaac Caswell :)
+#==============================================================================
+# CURRENT STATUS: In progress/ working! :) 
+#==============================================================================
+# USAGE: run from command line. if you want to use the toy example, run with
+# option -t
+#==============================================================================
+#
+###############################################################################
