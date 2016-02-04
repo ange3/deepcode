311c5d911c64a54ca76c052cd55a6ae344e771ad
Angela Sy <angelasy3@gmail.com>
2016-02-04 15:26:11 -0800

    added data map extractor file part 1

--- preprocess.py
+++ preprocess.py
@@ -1,3 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# File: preprocess.py
+# @Author: Angela Sy
+# @created: Feb 04 2016
+#
+#==============================================================================
+# DESCRIPTION: Pre-process data
+# 
+#==============================================================================
+# CURRENT STATUS: In progress
+#==============================================================================
+# USAGE:
+# import preprocess
+#==============================================================================
+#
+###############################################################################
+
 import features  # features.py (feature extractor file)
 import visualize  # visualize.py (visualizing data)
 
@@ -5,3 +24,4 @@ import visualize  # visualize.py (visualizing data)
 # Visualize Data
 # Extract Features
   # Note: To call a function from features.py, use syntax features.extractFeature()
+
