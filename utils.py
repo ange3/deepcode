21036c1aa46dba229ed128432a254949c6852f6d
Lisa Wang <lisa1010@stanford.edu>
2016-02-07 16:42:23 -0800

    added new data extraction method for our lasagne rnn

--- utils.py
+++ utils.py
@@ -48,3 +48,50 @@ def vectorize_syn_data(data_raw, num_timesteps):
     return X, y, corr
 
 
+def vectorize_data(data_raw):
+    """
+    inputs: 
+        - data_raw of shape (num_samples, num_problems)
+    outputs:
+        - X, which is the input to the RNN, shape(num_samples, num_timesteps, num_problems * 2)
+        - next_problem: for each student, indicates which problem the student is solving next. 
+                        one hot encoding : shape (num_samples, num_timesteps, num_problems)
+        - truth: for each student, indicates whether student answers problem at next time step
+               correctly.
+
+        notice that num_timesteps can be at most num_problems - 1, since we need 
+        to predict on the last input, whether student answers next problem correctly.
+    """
+
+    num_samples = data_raw.shape[0]
+    num_problems = data_raw.shape[1]
+    num_timesteps = num_problems - 1
+
+    X = np.zeros((num_samples, num_timesteps, num_problems * 2), dtype=np.bool)
+    next_problem = np.zeros((num_samples, num_timesteps, num_problems), dtype=np.int)
+    truth = np.zeros((num_samples, num_timesteps), dtype=np.int)
+
+    # Create 3-dimensional input tensor with one-hot encodings for each sample
+    # the dimension of each vector for a student i and time t is 2 * num_problems
+    # where the first half corresponds to the correctly answered problems and the
+    # second half to the incorrectly answered ones.
+    for i in xrange(num_samples):
+        
+        # for the first time step. Done separately so we can populate the output 
+        # tensor at the same time, which is shifted back by 1.
+
+        for t in xrange(0,num_timesteps):
+            p = t # since timestep t corresponds to problem p where t=p
+            if data_raw[i,p] == 1:
+                X[i, t, p] = 1 
+            else:
+                X[i, t, num_problems + p] = 1
+            # this is a special case for the synthetic data set, where the next problem 
+            # is just the current problem index + 1
+            next_problem[i,t, p+1] = 1
+            # truth tells us whether student i answers problem at next time step correctly
+            truth[i,t] = data_raw[i,t+1] 
+    # truth = np.copy(data_raw) 
+    # truth = truth[:, 1:]
+    return X, next_problem, truth
+
