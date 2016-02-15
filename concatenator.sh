07bd7bc2ef67a1f5e8a8146447be17b01fc1d146
hrlarry <hrlarry@stanford.edu>
2016-02-15 06:26:56 -0800

    improved concatenator bash script

--- concatenator.sh
+++ concatenator.sh
@@ -1,3 +1,3 @@
-tail -n +1 [0-9][0-9]*.txt > concatenated.csv
-sed -i "1s/^==> //" concatenated.csv
-((sed ':a;N;$!ba;s/\n/,/g' <concatenated.csv) | sed "s/,==> /\n/g") | sed "s/.txt <==//g" > concatenated.csv
+tail -n +1 [0-9][0-9]*.txt > concatenated.txt
+sed -i "1s/^==> //" concatenated.txt
+((sed ':a;N;$!ba;s/\n/,/g' <concatenated.txt) | sed "s/,==> /\n/g") | sed "s/.txt <==//g" > concatenated_test.csv
