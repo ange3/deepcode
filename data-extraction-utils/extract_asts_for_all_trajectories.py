7f0b1c85d77c7c964363808c9051e57800ac3982
Angela Sy <angelasy3@gmail.com>
2016-02-16 01:09:14 -0800

    WIP parsing larry's trajectories files and using counts to cut out freq 1 trajectories

--- data-extraction-utils/extract_asts_for_all_trajectories.py
+++ data-extraction-utils/extract_asts_for_all_trajectories.py
@@ -12,19 +12,29 @@ PATH_TO_ALL_PROBLEMS ='../data/hoc1-9/'
 PATH_TO_TRAJECTORIES = '/trajectories/'
 PATH_TO_AST_COUNTS = '/asts/counts.txt'
 
+PATH_TO_LARRY_TRAJECTORIES = '../data/trajectory_ast_csv_files/'
+LARRY_TRAJECTORIES_FILENAME_PRE = 'Trajectory_ASTs_'
+LARRY_TRAJECTORIES_FILENAME_POST = '.csv'
+
+PATH_TO_COUNTS = '../data/trajectory_count_files/'
+COUNTS_FILENAME_PRE = 'counts_'
+COUNTS_FILENAME_POST = '.txt'
+
 PATH_TO_PROCESSED_DATA = '../processed_data/'
 
 START_PROBLEM_ID = 1
 END_PROBLEM_ID = 1
 
 CLIP_TRAJECTORY_LENGTH = 20
+CLIP_TRAJECTORY_WITH_NUM_COUNT_THRESHOLD = 1
 
 
 def count_total_asts(hoc_num):
     '''
     Counts the number of unique ASTs present for this problem
     '''
-    ast_counts_filename = PATH_TO_ALL_PROBLEMS + 'hoc' + str(hoc_num) + PATH_TO_AST_COUNTS
+    # ast_counts_filename = PATH_TO_ALL_PROBLEMS + 'hoc' + str(hoc_num) + PATH_TO_AST_COUNTS
+    ast_counts_filename = PATH_TO_COUNTS + COUNTS_FILENAME_PRE + str(hoc_num) + COUNTS_FILENAME_POST
     counts = 0
     with open(ast_counts_filename, 'r') as ac_f:
         for l in ac_f:
@@ -79,6 +89,78 @@ def extract_asts_for_one_hoc(hoc_num):
     return trajectories_matrix
 
 
+def get_set_of_trajs_to_remove(hoc_num, freq_threshold):
+    '''
+    Return set of Trajectory IDs with a count less than or equal to freq_threshold
+    Args: hoc_num is the problem number
+          freq is the count threshold to determine if the trajectory id will be put in the removal set
+    '''
+
+    print 'Clip trajectories with count <= {}'.format(freq_threshold)
+
+    filepath = PATH_TO_COUNTS + COUNTS_FILENAME_PRE + str(hoc_num) + COUNTS_FILENAME_POST
+
+    traj_id_to_remove_set = set()
+
+    with open(filepath, 'rb') as count_file:
+        n_count = 0
+        for line in count_file:
+            traj_id, count = line.split() # tokenize over white space
+            # print traj_id, count
+            # if n_count <= 10:
+            #     print traj_id, count
+            #     n_count += 1
+            if int(count) <= freq_threshold:
+                traj_id_to_remove_set.add(traj_id)
+
+
+    print 'Removing {} Trajectories'.format(len(traj_id_to_remove_set))
+    return traj_id_to_remove_set
+
+def extract_asts_for_one_hoc_from_larry_trajectories(hoc_num):
+    traj_id_to_remove_set = get_set_of_trajs_to_remove(hoc_num, CLIP_TRAJECTORY_WITH_NUM_COUNT_THRESHOLD)
+    filepath = PATH_TO_LARRY_TRAJECTORIES + LARRY_TRAJECTORIES_FILENAME_PRE + str(hoc_num) + LARRY_TRAJECTORIES_FILENAME_POST
+
+    unique_asts_set = set()
+
+    raw_trajectories_list = []
+    longest_trajectory_len = -1
+    num_trajectories = 0
+
+    ncount=0
+    with open(filepath, 'rb') as traj_file:
+        for line in csv.reader(traj_file, delimiter=','):
+            traj_id = line[0]
+            if traj_id in traj_id_to_remove_set:
+                # print 'skipping ', traj_id
+                pass
+            raw_trajectory = np.array(list(line[1:]))
+            # if traj_id == '9999':
+            print raw_trajectory
+            print raw_trajectory.shape[0]
+            #     ncount+=1
+            # else:
+            #     exit(0)
+
+            for ast in raw_trajectory:
+                unique_asts_set.add(ast)
+         
+            longest_trajectory_len = max(longest_trajectory_len, raw_trajectory.shape[0])
+            longest_trajectory_id = traj_id
+            raw_trajectories_list.append(raw_trajectory)
+            num_trajectories += 1
+    
+    print 'Longest Trajectory Length: {} # ASTs at Traj ID: {}'.format(longest_trajectory_len, longest_trajectory_id)
+    print 'Num Trajectories: {}'.format(num_trajectories)
+
+    num_asts = count_total_asts(hoc_num)     
+    print 'Num ASTS Original: {}'.format(num_asts)
+    print 'Num ASTS after clipping trajectories: {}'.format(len(unique_asts_set))
+
+    trajectories_matrix = np.zeros((num_trajectories, longest_trajectory_len, num_asts))
+    max_timesteps = 0
+    
+
 def extract_asts_for_all_hocs():
     '''
     Extracts trajectories matrix for all problems and saves them to a numpy file
@@ -96,7 +178,7 @@ def extract_asts_for_all_hocs():
 
 
 if __name__ == "__main__":
-    extract_asts_for_one_hoc(1)
+    extract_asts_for_one_hoc_from_larry_trajectories(3)
     # extract_asts_for_all_hocs()
 
 
