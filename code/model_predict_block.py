624bb346a017efeca1477e1ddb4e101f4595bc41
Lisa Wang <lisa1010@stanford.edu>
2016-03-03 00:28:42 -0800

    in progress of making rnn to predict next block:

---
+++ code/model_predict_block.py
@@ -0,0 +1,247 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# File: model_predict_block.py
+# @Author: Lisa Wang
+# @created: Feb 29 2016
+#
+#==============================================================================
+# DESCRIPTION:
+# model functions for predicting the block in an AST using RNNs.
+#==============================================================================
+# CURRENT STATUS: In progress/ working! :) 
+#==============================================================================
+# USAGE: 
+# import model_predict_block
+#==============================================================================
+#
+###############################################################################
+
+import lasagne
+import numpy as np
+import theano
+import theano.tensor as T
+import time
+import utils
+
+def _build_net_layers(num_timesteps, num_asts, hidden_size, learning_rate, grad_clip=10, dropout_p=0.0, num_lstm_layers=1):
+    print("Building network ...")
+   
+    # First, we build the network, starting with an input layer
+    l_in = lasagne.layers.InputLayer(shape=(None, num_timesteps, num_asts))
+    l_mask = lasagne.layers.InputLayer(shape=(None, num_timesteps), name='Mask input')
+
+    # We now build the LSTM layer which takes l_in as the input layer
+    # We clip the gradients at GRAD_CLIP to prevent the problem of exploding gradients.
+
+    l_lstm_forward = lasagne.layers.LSTMLayer(
+            l_in, hidden_size, grad_clipping=grad_clip,
+            nonlinearity=lasagne.nonlinearities.tanh, mask_input=l_mask)
+
+    l_lstm_backward = lasagne.layers.LSTMLayer(
+            l_in, hidden_size, grad_clipping=grad_clip,
+            nonlinearity=lasagne.nonlinearities.tanh, backwards=True, mask_input=l_mask)
+
+    l_lstm = lasagne.layers.ConcatLayer(
+    [l_lstm_forward, l_lstm_backward], axis=-1, name='Sum 1')
+    # The l_forward layer creates an output of dimension (batch_size, num_timesteps, hidden_size)
+
+
+    l_reshape = lasagne.layers.ReshapeLayer(l_lstm, (-1, hidden_size))
+    l_dropout = lasagne.layers.DropoutLayer(l_reshape, p=dropout_p, rescale=True)
+    l_out = lasagne.layers.DenseLayer(l_dropout,
+        num_units=num_asts,
+        W = lasagne.init.Normal(),
+        nonlinearity=lasagne.nonlinearities.softmax)
+    # just reshaping, so we have the num_timesteps dimension back.
+    l_out = lasagne.layers.ReshapeLayer(l_out, (-1, num_timesteps, num_asts))
+    # after l_out, we have shape (batchsize, num_timesteps, num_asts)
+    # and the values should be probabilities for the asts
+    # for each trajectory, at each time step, we compute probabilities
+    # over all asts. The probabilities should sum up to 1, that's why we use
+    # softmax nonlinearity.
+    l_out_slice = lasagne.layers.SliceLayer(l_out, indices=-1, axis=1)
+    l_lstm_slice = lasagne.layers.SliceLayer(l_lstm, indices=-1, axis=1)
+    
+    return l_in, l_out, l_out_slice, l_lstm, l_lstm_slice
+
+
+def create_model(num_timesteps, num_asts, hidden_size, learning_rate, grad_clip=10, dropout_p=0.5, num_lstm_layers=1):
+    '''
+     returns train function which reports both loss and accuracy
+     and test function, which also reports both loss and accuracy
+    '''
+    
+    l_in, l_out, l_out_slice, l_lstm, l_lstm_slice = _build_net_layers(num_timesteps, num_asts, hidden_size, learning_rate, grad_clip, dropout_p, num_lstm_layers)
+    # pred should be of shape (batchsize, num_timesteps, num_asts)
+    pred = lasagne.layers.get_output(l_out)
+    # pred_slice should be of shape (batchsize, num_asts), only contains
+    # predictions for the last timestep
+    pred_slice = lasagne.layers.get_output(l_out_slice)
+    # the hidden representations for the last timestep (batchsize, hidden_size)
+    hidden_slice = lasagne.layers.get_output(l_lstm_slice)
+    # truth should also be of shape (batchsize, num_timesteps, num_asts)
+    truth = T.imatrix("truth")
+    mask = T.imatrix("mask")
+    pred_2d = pred.reshape((-1, num_asts))
+    truth_1d = truth.reshape((-1,))
+
+    # pred_2d_shape = T.shape(pred_2d)
+    # truth_1d_shape = T.shape(truth_1d)
+
+    # categorical_crossentropy
+    loss = T.nnet.categorical_crossentropy(pred_2d, truth_1d).mean()
+    # categorical accuracy
+    # acc = T.nnet.categorical_crossentropy(pred_2d, truth_1d).mean()
+    acc = lasagne.objectives.categorical_accuracy(pred_2d, truth_1d).mean()
+    # update function
+    print("Computing updates ...")
+    all_params = lasagne.layers.get_all_params(l_out)
+    updates = lasagne.updates.adam(loss, all_params, learning_rate)
+
+    # training function
+    print("Compiling functions ...")
+    train_loss = theano.function([l_in.input_var, l_mask.input_var, truth], loss, updates=updates, allow_input_downcast=True)
+    compute_loss = theano.function([l_in.input_var, l_mask.input_var, truth], loss, allow_input_downcast=True)
+    # training function, returns loss and acc
+    train_loss_acc = theano.function([l_in.input_var, l_mask.input_var, truth], [loss, acc, pred], updates=updates, allow_input_downcast=True)
+    # computes loss and accuracy, without training
+    compute_loss_acc = theano.function([l_in.input_var, l_mask.input_var, truth], [loss, acc, pred], allow_input_downcast=True)
+
+    # In order to generate text from the network, we need the probability distribution of the next character given
+    # the state of the network and the input (a seed).
+    # In order to produce the probability distribution of the prediction, we compile a function called probs. 
+    probs = theano.function([l_in.input_var, l_mask.input_var], pred_slice, allow_input_downcast=True)
+
+    generate_hidden_representations = theano.function([l_in.input_var, l_mask.input_var], l_lstm_slice, allow_input_downcast=True)
+
+    print("Compiling done!")
+    
+    return train_loss_acc, compute_loss_acc, probs, generate_hidden_representations
+
+
+def train(train_data, val_data, train_loss_acc, compute_loss_acc, num_epochs=5, batchsize=32, record_per_iter=False):
+    X_train, mask_train, truth_train = train_data
+    X_val, mask_val, truth_val = val_data
+
+    num_train = X_train.shape[0]
+    num_val = X_val.shape[0]
+    total_train_iters = (num_train / batchsize) * num_epochs
+    num_iters = 0
+
+
+    print("Starting training...")
+    print("Total training iterations: {}".format(total_train_iters))
+    # We iterate over epochs:
+    train_accs = []
+    val_accs = []
+    train_corrected_accs = []
+    val_corrected_accs = []
+    train_losses = []
+    val_losses = []
+
+    for epoch in range(num_epochs):
+        # In each epoch, we do a full pass over the training data:
+        train_loss_ep = 0.0
+        train_acc_ep = 0.0
+        train_corrected_acc_ep = 0.0
+        train_batches = 0
+        start_time = time.time()
+        for batch in utils.iter_minibatches(train_data, batchsize, shuffle=False):
+            X_, truth_ = batch
+            train_loss, train_acc, train_pred = train_loss_acc(X_, truth_)
+            train_corrected_acc = compute_corrected_acc_on_ast_rows(X_, truth_, train_pred)
+            num_iters += 1
+            val_loss, val_acc, val_corrected_acc, pred_val = _compute_loss_acc_pred(X_val, mask_val, truth_val, compute_loss_acc)
+            print("Ep {} \titer {}  \tloss {:.5f}, train acc {:.2f}, train corr acc {:.2f}, val acc {:.2f}, val corr acc {:.2f}".format(epoch, num_iters, float(train_loss), train_acc * 100, train_corrected_acc * 100, val_acc *100, val_corrected_acc *100) )
+        
+            if record_per_iter:
+                train_accs.append(train_acc)
+                train_corrected_accs.append(train_corrected_acc)
+                train_losses.append(train_loss)
+                val_losses.append(val_loss)
+                val_accs.append(val_acc)
+                val_corrected_accs.append(val_corrected_acc)
+
+        # train_acc = train_acc_ep/train_batches
+        # train_corrected_acc = train_corrected_acc_ep/train_batches
+        # train_loss = train_loss_ep/train_batches
+        
+        # train_loss, train_acc, train_corrected_acc, pred_train = _compute_loss_acc_pred(X_train[:num_val,:,:], truth_train[:num_val,:], compute_loss_acc)
+        train_loss, train_acc, train_corrected_acc, pred_train = _compute_loss_acc_pred(X_train, mask_train, truth_train, compute_loss_acc)        
+        if not record_per_iter:
+            # recording values for each epoch
+            train_accs.append(train_acc)
+            train_corrected_accs.append(train_corrected_acc)
+            train_losses.append(train_loss)
+            val_losses.append(val_loss)
+            val_accs.append(val_acc)
+            val_corrected_accs.append(val_corrected_acc)
+
+        # Then we print the results for this epoch:
+        print("\nEpoch {} of {} took {:.3f}s".format(epoch + 1, num_epochs, time.time() - start_time))
+        print("  training loss:\t\t{:.6f}".format(float(train_loss)))
+        print("  training raw accuracy:\t\t{:.2f} %".format(train_acc * 100))
+        print("  training corrected acc:\t\t{:.2f} %".format(train_corrected_acc * 100))
+
+        print("  validation loss:\t\t{:.6f}".format(float(val_loss)))
+        print("  validation raw accuracy:\t\t{:.2f} %".format(val_acc * 100))
+        print("  validation corrected acc:\t\t{:.2f} % \n".format(val_corrected_acc * 100))
+
+    print("Training completed.")
+    return train_losses, train_accs, train_corrected_accs, val_losses, val_accs, val_corrected_accs
+
+
+def _compute_loss_acc_pred(X, mask, truth, compute_loss_acc):
+    # a full pass over the given data without training
+    # returns loss, raw accuracy, corrected accuracy and predictions
+    loss, acc, pred = compute_loss_acc(X, mask, truth)
+    # corrected_acc = compute_corrected_acc_on_ast_rows(X, mask, truth, pred)
+    return loss, acc, corrected_acc, pred
+
+
+def compute_corrected_acc_on_ast_rows(X, truth, pred):
+    batchsize, num_timesteps, num_asts = X.shape
+    correct_count = 0
+    total_count = 0
+    for n in xrange(batchsize):
+        for t in xrange(num_timesteps):
+            if np.argmax(X[n,t,:]) != 0:
+                # only include predictions not on the <END> token
+                total_count += 1
+                if np.argmax(pred[n,t,:]) == truth[n,t]:
+                    correct_count += 1
+    corrected_acc = correct_count/float(total_count)
+    return corrected_acc
+
+
+def compute_corrected_acc_on_ast_ids(X_ast_ids, truth_ast_ids, pred_ast_ids): 
+    batchsize, num_timesteps = X_ast_ids.shape
+    correct_count = 0
+    total_count = 0
+    for n in xrange(batchsize):
+        for t in xrange(num_timesteps):
+            if X_ast_ids[n,t] != -1:
+                # only include predictions not on the <END> token
+                total_count += 1
+                if pred_ast_ids[n,t] == truth_ast_ids[n,t]:
+                    correct_count += 1
+    corrected_acc = correct_count/float(total_count)
+    return corrected_acc
+
+
+def check_accuracy(data, compute_loss_acc, row_to_ast_id_map, dataset_name):
+    X, mask, truth = data
+    print("Testing...")
+    # After training, we compute and print the test error:
+    loss, acc, pred = compute_loss_acc(X, mask, truth)
+    # loss, raw_acc, corrected_acc, pred = _compute_loss_acc_pred(X, mask, truth, compute_loss_acc)
+
+    print("Final results:")
+    print("  {} loss:\t\t\t{:.6f}".format(dataset_name, loss * 1.0))
+    print("  {} accuracy:\t\t{:.2f} %".format(dataset_name, acc * 100))
+    print("  {} raw accuracy:\t\t{:.2f} %".format(dataset_name, raw_acc * 100))
+    print("  {} corrected accuracy:\t{:.2f} %".format(dataset_name, corrected_acc * 100))
+
+    return loss, raw_acc, corrected_acc, pred
+
--- code/utils.py
+++ code/utils.py
@@ -19,7 +19,31 @@
 
 import numpy as np
 import time
+import pickle
+import random
 
+
+# Each trajectory matrix corresponds to one hoc exercise and is
+# its own data set. Mixing data sets currently does not make much
+# sense since the AST IDs don't persist betweeen different hoc's.
+TRAJ_MAP = {
+    'hoc1': '../processed_data/traj_matrix_1.npy',
+    'hoc2': '../processed_data/traj_matrix_2.npy',
+    'hoc3': '../processed_data/traj_matrix_3.npy',
+    'hoc4': '../processed_data/traj_matrix_4.npy',
+    'hoc5': '../processed_data/traj_matrix_5.npy',
+    'hoc6': '../processed_data/traj_matrix_6.npy',
+    'hoc7': '../processed_data/traj_matrix_7.npy',
+    'hoc8': '../processed_data/traj_matrix_8.npy',
+    'hoc9': '../processed_data/traj_matrix_9.npy' 
+}
+
+TRAJ_MAP_PREFIX = '../processed_data/traj_matrix_'
+TRAJ_MAP_SUFFIX = '.npy'
+
+
+BLOCK_MAT_PREFIX = '../processed_data/ast_matrix_'
+MAT_SUFFIX = '.npy'
 
 
 # ############################# Batch iterator ###############################
@@ -155,10 +179,7 @@ def prepare_traj_data_for_rnn(raw_matrix):
         - raw_matrix of shape (num_traj, max_traj_len, num_asts)
     outputs:
         - X, which is the input to the RNN, shape(num_traj, num_timesteps, num_asts)
-        - y: truth vector.
-            for each trajetory, indicates whether student answers problem at next time step
-               correctly. shape(num_samples, num_timesteps)
-               shape(num_traj, num_timesteps, num_asts)
+        - y: truth vector. shape(num_traj, num_timesteps, num_asts)
     """
     (num_traj, max_traj_len, num_asts) = raw_matrix.shape
     # notice that num_timesteps can be at most max_traj_len - 1, since we need 
@@ -178,6 +199,43 @@ def prepare_traj_data_for_rnn(raw_matrix):
             y[n,t] = np.argmax(raw_matrix[n,t+1,:])
 
     return X, y
+
+def prepare_block_data_for_rnn(raw_matrix):
+    """
+    inputs: 
+        - raw_matrix of shape (num_traj, max_traj_len, num_asts)
+    outputs:
+        - X, which is the input to the RNN, shape(num_asts, num_timesteps, num_blocks)
+        - mask, which masks out all the values in the X tensor which don't
+            correspond to any sequence. (since some sequences are shorter 
+                than num_timesteps), shape(num_asts, num_timesteps)
+        - y: truth vector. the correct next block.
+               shape(num_asts, num_timesteps)
+    """
+    (num_asts, max_ast_len, num_blocks) = raw_matrix.shape
+    # notice that num_timesteps can be at most max_traj_len - 1, since we need 
+    # to be able to predict on the last input and have a truth value.  
+    num_timesteps = max_ast_len - 1
+
+    X = np.copy(raw_matrix[:,:-1,:])
+    mask = np.ones((num_asts, num_timesteps))
+    
+    for n in xrange(num_asts):
+        for t in xrange(num_timesteps):
+            # if the ast block sequence already ended (as indicated by dummy block
+            # at index 0, then mask out)
+            if X[n,t,0] == 1:
+                mask[n,t] = 0
+
+    # y has shape (num_asts, num_timesteps)
+    # and the values are the indices corresponding to the correct ast prediction
+    # instead of one-hot encoding
+    y = np.zeros((num_asts, num_timesteps))
+    for n in xrange(num_asts):
+        for t in xrange(num_timesteps):
+            y[n,t] = np.argmax(raw_matrix[n,t+1,:])
+
+    return X, mask, y
 
 def convert_data_to_ast_ids(data, row_to_ast_id_map):
 
@@ -206,6 +264,33 @@ def convert_data_to_ast_ids(data, row_to_ast_id_map):
 
     return X_ast_ids, y_ast_ids
 
+def convert_ast_or_block_data_to_ids(X, y, row_to_id_map):
+
+    '''
+    INPUT:
+    data = (X,y)
+    X: (batchsize, num_timesteps, num_asts)
+    y: (batchsize, num_timestep)
+
+    OUTPUT:
+    X_ast_ids: (batchsize, num_timesteps), containing ast ids, which we can
+            use to look up ast json files.
+    y_ast_ids: (batchsize, num_timesteps)
+
+    '''
+    batchsize, num_timesteps, _ = X.shape
+    X_ids = np.zeros((batchsize, num_timesteps))
+    y_ids = np.zeros((batchsize, num_timesteps))
+
+    for n in xrange(batchsize):
+        for t in xrange(num_timesteps):
+            y_ids[n,t] = row_to_id_map[int(y[n,t])]
+            X_ids[n,t] = row_to_id_map[np.argmax(X[n,t,:])]
+
+    return X_ids, y_ids
+
+
+
 def convert_pred_to_ast_ids(pred, row_to_ast_id_map):
     batchsize, num_timesteps, num_asts = pred.shape
     pred_ast_ids = np.zeros((batchsize, num_timesteps))
@@ -215,34 +300,116 @@ def convert_pred_to_ast_ids(pred, row_to_ast_id_map):
     
     return pred_ast_ids
 
-# def prepare_traj_data_for_rnn(raw_matrix):
-#     """
-#     inputs: 
-#         - raw_matrix of shape (num_traj, max_traj_len, num_asts)
-#     outputs:
-#         - X, which is the input to the RNN, shape(num_traj, num_timesteps, num_asts)
-#         - y: truth vector.
-#             for each trajetory, indicates whether student answers problem at next time step
-#                correctly. shape(num_samples, num_timesteps)
-#                shape(num_traj, num_timesteps, num_asts)
-#     """
-#     (num_traj, max_traj_len, num_asts) = raw_matrix.shape
-#     # notice that num_timesteps can be at most max_traj_len - 1, since we need 
-#     # to be able to predict on the last input and have a truth value.  
-#     num_timesteps = max_traj_len - 1
 
-#     X = np.copy(raw_matrix[:,:-1,:])
+def load_dataset_predict_ast(hoc_num=7, data_sz=-1):
+    print('Preparing network inputs and targets, and the ast maps...')
+    hoc_num = str(hoc_num)
+    data_set = 'hoc' + hoc_num
+    # if DATA_SZ = -1, use entire data set
+    # For DATA_SZ, powers of 2 work best for performance.
+    ast_map_file = '../processed_data/map_ast_row_' + hoc_num + '.pickle'
 
-#     # y = np.copy(raw_matrix[:,1:,:])
+    # Load AST ID to Row Map
+    ast_id_to_row_map = pickle.load(open( ast_map_file, "rb" ))
+    # Create Row to AST ID Map by inverting the previous one
+    row_to_ast_id_map = {v: k for k, v in ast_id_to_row_map.items()}
+    # trajectories matrix for a single hoc exercise
+    # shape (num_traj, max_traj_len, num_asts)
+    # Note that ast_index = 0 corresponds to the <END> token,
+    # marking that the student has already finished.
+    # The <END> token does not correspond to an AST.
+    traj_mat = np.load(TRAJ_MAP[data_set])
 
-#     # alternative version where y has shape (num_traj, num_timesteps)
-#     # and the values are the indices corresponding to the correct ast prediction
-#     # instead of one-hot encoding
-#     y = np.zeros((num_traj, num_asts))
-#     y = np.copy(raw_matrix[:,-1,:])
-#     # for n in xrange(num_traj):
-#     #     y[:] = np.argmax(raw_matrix[n,-1,:])
+    # if data_sz specified, reduce matrix. 
+    # Useful to create smaller data sets for testing purposes.
+    if data_sz != -1:
+        traj_mat = traj_mat[:data_sz]
+    print 'Trajectory matrix shape {}'.format(traj_mat.shape)
 
-#     return X, y
+    # shuffle the first dimension of the matrix
+    np.random.shuffle(traj_mat)
 
+    num_traj, max_traj_len, num_asts = traj_mat.shape
+    # Split data into train, val, test
+    # TODO: Replace with kfold validation in the future
+    # perhaps we can use sklearn kfold?
+    train_mat = traj_mat[0:7*num_traj/8,:]
+    val_mat =  traj_mat[7*num_traj/8: 15*num_traj/16 ,:]
+    test_mat = traj_mat[15*num_traj/16:num_traj,:]
+
+    train_data = prepare_traj_data_for_rnn(train_mat)
+    val_data = prepare_traj_data_for_rnn(val_mat)
+    test_data = prepare_traj_data_for_rnn(test_mat)
+
+
+    X_train, y_train = train_data
+    X_val, y_val = val_data
+    X_test, y_test = test_data
+    print 'X_train shape {}'.format(X_train.shape)
+    print 'y_train shape {}'.format(y_train.shape)
+    print 'X_val shape {}'.format(X_val.shape)
+    print 'X_test shape {}'.format(X_test.shape)
+    num_train, num_timesteps, num_asts = X_train.shape
+
+    # print y_train[:10]
+    # print X_train[:10,:, :10]
+
+    X_train_ast_ids, y_train_ast_ids = convert_data_to_ast_ids(train_data, row_to_ast_id_map)
+    # print X_train_ast_ids[:10]
+    # print y_train_ast_ids[:10]
+    print 'num_timesteps {}'.format(num_timesteps)
+    print ("Inputs and targets done!")
+    return train_data, val_data, test_data, ast_id_to_row_map, row_to_ast_id_map, num_timesteps, num_asts
+
+
+def load_dataset_predict_block(hoc_num=7, data_sz=-1):
+    print('Preparing network inputs and targets, and the block maps...')
+    hoc_num = str(hoc_num)
+    data_set = 'hoc' + hoc_num
+    # if DATA_SZ = -1, use entire data set
+    # For DATA_SZ, powers of 2 work best for performance.
+
+
+    # block_map_file = '../processed_data/map_block_string_to_block_id' + hoc_num + '.pickle'
+
+    # # Load AST ID to Row Map
+    # block_id_to_row_map = pickle.load(open( block_map_file, "rb" ))
+    # # Create Row to AST ID Map by inverting the previous one
+    # row_to_block_id_map = {v: k for k, v in block_id_to_row_map.items()}
+
+
+    # trajectories matrix for a single hoc exercise
+    # shape (num_traj, max_traj_len, num_blocks)
+    # Note that block_index = 0 corresponds to the <END> token,
+    # marking that the student has already finished.
+    # The <END> token does not correspond to an AST.
     
+    ast_mat = np.load(BLOCK_MAT_PREFIX + hoc_num + MAT_SUFFIX)
+
+    # if data_sz specified, reduce matrix. 
+    # Useful to create smaller data sets for testing purposes.
+    if data_sz != -1:
+        ast_mat = ast_mat[:data_sz]
+    print 'Trajectory matrix shape {}'.format(ast_mat.shape)
+
+    # shuffle the first dimension of the matrix
+    np.random.shuffle(ast_mat)
+
+    num_asts, max_ast_len, num_blocks = ast_mat.shape
+    # Split data into train, val, test
+    # TODO: Replace with kfold validation in the future
+    # perhaps we can use sklearn kfold?
+    train_mat = ast_mat[0:7*num_asts/8,:]
+    val_mat =  ast_mat[7*num_asts/8: 15*num_asts/16 ,:]
+    test_mat = ast_mat[15*num_asts/16:num_asts,:]
+
+    train_data = prepare_block_data_for_rnn(train_mat)
+    val_data = prepare_block_data_for_rnn(val_mat)
+    test_data = prepare_block_data_for_rnn(test_mat)
+
+    num_timesteps = train_data[0].shape[1]
+
+    print ("Inputs and targets done!")
+    # return train_data, val_data, test_data, block_id_to_row_map, row_to_block_id_map, num_timesteps, num_blocks
+    return train_data, val_data, test_data, num_timesteps, num_blocks
+
